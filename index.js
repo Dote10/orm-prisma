202d80064c7c990e86f3602a689f3ac5616b5091
surge100 <zoqol@daum.net>
2024-04-12 14:00:36 +0900

    프리즈마를 사용하여 CRUD구현

--- index.js
+++ index.js
@@ -19,6 +19,38 @@ app.get('/',(req,res) =>{
     res.send("Hello prisma");
 });
 
+app.get('/articles', async (req,res) =>{
+    //3.특정 조건에 부합하는 기사들 검색
+    //(fetch all article in draft state)
+    const articles = await prisma.article.findMany({
+        where:{
+            state:req.query.state
+        }
+    });
+
+    res.json(articles);
+
+});
+
+app.get('/articles', async(req,res) =>{
+    //1.모든 기사 검색하기 
+    const articles = await prisma.article.findMany();
+    res.json(articles);
+})
+
+
+app.get('/articles/:id', async(req,res) =>{
+    //2.특정 기사 하나 검색 
+    const article = await prisma.article.findFirst({
+        where: {
+            id: +req.params.id
+        }
+    })
+
+    res.json(article);
+})
+
+
 app.post('/articles', async (req, res) =>{
     await prisma.article.create({
         data : req.body 
@@ -26,12 +58,40 @@ app.post('/articles', async (req, res) =>{
     res.json({success:true});
 });
 
+
 app.post('/multiple-articles', async (req, res) =>{
     await prisma.article.createMany({
         data: req.body
     });
     res.json({success:true});
 });
+
+//article update
+app.put('/articles/:ids', async(req, res) =>{
+const idList = req.params.ids.split(',').map(id => +id);
+const updatedCount = await prisma.article.updateMany({
+   where:{
+       id:{
+           in : idList
+       }
+   },
+   data:{
+       title: req.body.title
+   }
+});
+    res.json(updatedCount);
+});
+
+//article delete
+app.delete('/articles/:id', async (req,res) =>{
+    const deleted = await prisma.article.delete({
+        where:{
+            id: +req.params.id
+        }
+    });
+
+    res.json(deleted);
+})
 
 app.listen(7500, () => {
     console.log("App working");
